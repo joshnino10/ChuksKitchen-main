1e9c6202428ea0f994200b50f3d9d72890821a80
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-03-15 13:44:45 +0100

    j

--- Component/CustomSplashScreen/CustomSplashScreen.tsx
+++ Component/CustomSplashScreen/CustomSplashScreen.tsx
@@ -1,51 +1,22 @@
-import { useRouter } from 'expo-router';
-import React, { useEffect, useRef } from 'react';
-import { Animated, Dimensions, StatusBar, StyleSheet, View } from 'react-native';
+
+import React from 'react';
+import { Dimensions, StatusBar, StyleSheet, View, Image } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
 export default function CustomSplashScreen() {
-  const router = useRouter();
-
-  // Animated values for drift
-  const translateY = useRef(new Animated.Value(0)).current;
-  const translateX = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    // Drift vertically
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(translateY, { toValue: -20, duration: 4000, useNativeDriver: true }),
-        Animated.timing(translateY, { toValue: 0, duration: 4000, useNativeDriver: true }),
-      ])
-    ).start();
-
-    // Drift horizontally
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(translateX, { toValue: 15, duration: 6000, useNativeDriver: true }),
-        Animated.timing(translateX, { toValue: 0, duration: 6000, useNativeDriver: true }),
-      ])
-    ).start();
-
-  }, []);
+ 
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
-      <Animated.Image
+
+      <Image
         source={require('../../assets/images/Splahscreen.png')}
-        style={[
-          styles.image,
-          {
-            transform: [
-              { translateY: translateY },
-              { translateX: translateX },
-            ],
-          },
-        ]}
+        style={styles.image}
         resizeMode="cover"
       />
+
     </View>
   );
 }
@@ -55,7 +26,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    width: width + 40,   // extra width for horizontal drift
-    height: height + 40, // extra height for vertical drift
+    width: width,
+    height: height,
   },
 });
